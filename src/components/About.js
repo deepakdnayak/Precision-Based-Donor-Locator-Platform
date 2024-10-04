81fc919dc3aee70a281196e268636ff59582bd81
Praveena Prakash Gunaga <gunagapraveena@gmail.com>
2024-10-04 12:05:17 +0530

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div className="container" style={{ marginTop: "80px" }}>
             <h1>This page contains information on About</h1>
+            <p>this is about</p>
         </div>
     )
 }
